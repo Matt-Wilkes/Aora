e22a0fba83a910ed4e6c09a13acd44d7a1122aa7
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-09-08 21:25:32 +0100

    build(onboarding): add CustomButton component, add functionality to sign-in button

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Text, View, Image } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
     // Ensures contact doesn't overlap based on device
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-centere items-center h-full px-4">
+      {/* min height of screen 85vh */}
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -32,8 +34,17 @@ export default function App() {
             />
           </View>
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+
+          <CustomButton 
+          title="Continue with Email"
+          // on press, navigate to sign-in
+          handlePress={() => {router.push('/sign-in')}}
+            containerStyles={"w-full mt-7"}
+          ></CustomButton>
         </View>
       </ScrollView>
+      {/* changing how the time, battery wifi etc are displayed */}
+      <StatusBar backgroundColor="#161622" style='light'/> 
     </SafeAreaView>
   );
 }
