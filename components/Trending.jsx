9aa4bae3acc41fddb3463c220c1eb3f3db3b6f09
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-09-13 16:42:33 +0100

    feat(home videos): add video component to home screen with playback

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -2,19 +2,20 @@ import { View, Text, FlatList, TouchableOpacity, ImageBackground, Image } from '
 import {React, useState} from 'react'
 import * as Animatable from 'react-native-animatable'
 import { icons } from '../constants'
+import { Video, ResizeMode } from 'expo-av';
 
 const zoomIn = {
   0: {
     scale: 0.9,
   },
   1: {
-    scale: 1,
+    scale: 1.1,
   }
 }
 
 const zoomOut = {
   0: {
-    scale: 1,
+    scale: 1.1,
   },
   1: {
     scale: 0.9,
@@ -24,7 +25,7 @@ const zoomOut = {
 const TrendingItem = ({ activeItem, item}) => {
   const [play, setPlay] = useState(false);
 
-  // console.log(activeItem, item.$id)
+  console.log(activeItem, item.$id, item.video)
 
   return (
     <Animatable.View
@@ -33,7 +34,21 @@ const TrendingItem = ({ activeItem, item}) => {
     duration={500}
     >
     {play ? (
-      <Text className="text-white">Playing</Text>
+      <Video
+        source={{
+          uri: item.video,
+        }}
+        className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        shouldPlay
+        onPlaybackStatusUpdate={(status) => {
+          if(status.didJustFinish){
+            setPlay(false);
+          }
+        }}
+        
+      />
     ) : (
       <TouchableOpacity className="relative justify-center items-center" activeOpacity={0.7}
       onPress={() => setPlay(true)}>
@@ -59,10 +74,9 @@ const TrendingItem = ({ activeItem, item}) => {
 const Trending = ({posts}) => {
   // initial state is the second post
   const [activeItem, setActiveItem] = useState(posts[1]);
-  // console.log(`posts object: ${posts[1]}`)
-  // console.log(`posts 1 id: ${posts[1].$id}`)
+
   const viewableItemsChanged = ({ viewableItems }) => {
-    console.log(`viewable items: ${viewableItems[0]}`)
+    // console.log(`viewable items: ${viewableItems[0]}`)
     if (viewableItems.length > 0) {
       // set ActiveItem as the key
       setActiveItem(viewableItems[0].key)
